01d47a4edc97c4653cc93ba76fabc19546446456
911_Adam_Paul <72084877+pauladam2001@users.noreply.github.com>
2021-11-08 12:46:59 +0200

    New font size

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -9,7 +9,7 @@ const Logo = () => {
 		<Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >	
 			 <div className="Tilt-inner pa3">
 			 	<img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
-			 	<div className='pa3 f7'>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+			 	<div className='pa3 f7 logoText'>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 			 </div>
 		</Tilt>
 		</div>
